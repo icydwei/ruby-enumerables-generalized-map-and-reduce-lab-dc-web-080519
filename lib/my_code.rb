d08e6bec6b9ff4140d781d9bfce64f5ade06ff24
Rob Paik <robpaik@gmail.com>
2019-07-01 04:10:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  counter = 0
+  while counter < source_array.length do
+    new.push(yield(source_array[counter]))
+    counter +=1
+  end
+  return new
+end
+
+
+def reduce(source_array, starting_value=0)
+  truth = starting_value
+  counter = 0
+  while counter < source_array.length do
+    truth = yield(truth, source_array[counter])
+    counter +=1
+  end
+  return truth
+end
